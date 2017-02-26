779425e5e24a2efd44e51a99262fbd4e37b4809f
Johnxy <Johnxy@qq.com>
2017-02-27 00:08:12 +0800

    Update jData.js

--- jData.js
+++ jData.js
@@ -1,5 +1,32 @@
 /*
 * jData v1.0.0 - handle data, rely on Zeptojs
+* @description You have to change BASEURL as your server url.
+* @example
+    jData.add({
+      "creator" : "johnxy",
+      "content" : "Hello world!"
+    }, function(result){
+      console.log(result);
+    });
+    
+    jData.delete(1, function(result){
+      console.log(result);
+    });
+    
+    jData.query({
+      "keyword" : "keyword",
+      "pageNum" : 1,
+      "pageSize" : 10
+    }, function(result){
+      console.log(result);
+    });
+    
+    jData.update(1, {
+      "creator" : "johnxy",
+      "content" : "Hello world!"
+    }, function(result){
+      console.log(result);
+    });
 **/
 ;(function(global, factory) {
   if(typeof define === 'function' && define.amd) {
@@ -8,13 +35,16 @@
     factory(global);
   }
 }(this, function(window) {
-  var jData = {}, context = {},
-      BASEURL = '//***.com/superWelfare/';
+  
+  //you need to change BASEURL for reuqest.
+  var BASEURL = '//***.com/superWelfare/';
+  
+  var jData = {}, context = this;
   
   var request = function(action, data, callback){
     // request the server and trigger the callback event
     $.ajax({
-      url : BASEURL + $action,
+      url : BASEURL + action,
       data : data,
       success : callback || function(){
         console.info("Info:" + action + " operation is successful.");
@@ -27,19 +57,19 @@
   };
   
   var add = function(data, callback){
-    request.call(this, 'add', data, callback);
+    request.call(this, 'add', {"data" : data}, callback);
   };
   
   var delete = function(id, callback){
-    request.call(this, 'delete', data, callback);
+    request.call(this, 'delete', {"id" : id}, callback);
   };
   
   var query = function(data, callback){
-    request.call(this, 'query', data, callback);
+    request.call(this, {"keyword" : "keyword", "pageNum" : 1, "pageSize" : 10}, callback);
   };
   
   var update = function(id, data, callback){
-    request.call(this, 'update', data, callback);
+    request.call(this, 'update', {"id" : id, "data" : data}, callback);
   };
   
   jData = {
@@ -49,5 +79,6 @@
     "update" : update
   };
   
-  return jData;
+  return window.jData = jData;
+  
 }));
